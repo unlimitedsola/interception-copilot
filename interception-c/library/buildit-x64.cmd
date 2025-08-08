80ab3ce3dffaa301436d12e642eeb0138661e64d
Sola <dev@sola.love>
2025-08-08 10:33:09 +0800

    cmake

--- interception-c/library/buildit-x64.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-SET PATH=%PATH%;..\tools
-
-%ComSpec% /c "scd . && pushd . && %WDK%\bin\setenv %WDK% fre x64 WIN7 no_oacr && popd && build"
--- interception-c/library/buildit.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-SET PATH=%PATH%;..\tools
-
-%ComSpec% /c "scd . && pushd . && %WDK%\bin\setenv %WDK% fre x86 WXP no_oacr && popd && build"
